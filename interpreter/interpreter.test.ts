2934cb76a7cef1ca9819775f24c5264c3f5a67ad
Krzysztof Studniarek <krzysztof.studniarek@coinmetro.com>
2024-06-19 14:09:43 +0200

    feat: add logical validations

--- interpreter/interpreter.test.ts
+++ interpreter/interpreter.test.ts
@@ -1,5 +1,5 @@
 import { Interpreter } from "./interpreter";
-import { PUSH, ADD, SUB, MUL, STOP, JUMP, DIV} from "./operations/opCodes";
+import { PUSH, ADD, SUB, MUL, STOP, JUMP, DIV, AND} from "./operations/opCodes";
 
 test(`Test Interpreter`, () => {
     expect(new Interpreter().runCode([PUSH, 2, PUSH, 3, MUL, PUSH, 5, ADD, PUSH, 11, SUB, STOP])).toBe(0);
@@ -10,5 +10,9 @@ test(`Test Interpreter Errors`, () => {
     expect(() => new Interpreter().runCode([PUSH, 5, PUSH, "ala ma kota", DIV, STOP])).toThrow("requires two numbers");
     expect(() => new Interpreter().runCode([PUSH])).toThrow("PUSH can't be last operation in the code");
     expect(() => new Interpreter().runCode([PUSH, 0, PUSH, 5, DIV, STOP])).toThrow("Division by zero");
+    expect(() => new Interpreter().runCode([PUSH, 0, PUSH, 5, AND, STOP])).toThrow("");
+});
+
+test(`Test Interpreter execution limit`, () => {
     expect(() => new Interpreter().runCode([PUSH, 0, JUMP, STOP])).toThrow("Execution limit of 10000 exceeded");
 });
--- interpreter/operations/logical.ts
+++ interpreter/operations/logical.ts
@@ -1,4 +1,5 @@
-import { getValues } from "./opCodes";
+import { validateLogicalOperation } from "../validations/logical";
+import { AND, getValues, OR } from "./opCodes";
 
 export const handleEq = (state: any) => {
     const {a, b} = getValues(state);
@@ -7,10 +8,12 @@ export const handleEq = (state: any) => {
 
 export const handleAnd = (state: any) => {
     const {a, b} = getValues(state);
+    validateLogicalOperation(a, b, AND);
     state.stack.push(a && b);
 }
 
 export const handleOr = (state: any) => {
     const {a, b} = getValues(state);
+    validateLogicalOperation(a, b, OR);
     state.stack.push(a || b);
 }
