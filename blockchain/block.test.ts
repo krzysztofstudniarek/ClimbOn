1e866146910e796abcc4275e5d146e711523bdc6
Krzysztof Studniarek <krzysztof.studniarek@coinmetro.com>
2024-07-09 16:24:55 +0200

    feat: add one more test

--- blockchain/block.test.ts
+++ blockchain/block.test.ts
@@ -4,6 +4,21 @@ test(`Test Target Block Hash Calculation When Difficulty is 1`, () => {
     expect(Block.calculateBlockTargetHash({ lastBlock: Block.genesis() })).toBe('f'.repeat(64));
 });
 
+test(`Test Target Block Hash Calculation When Difficulty is 100`, () => {
+    expect(Block.calculateBlockTargetHash({
+        lastBlock: new Block({
+            blockHeaders: {
+                parentHash: '--test-parent-hash--',
+                beneficiary: '--test-beneficiary--',
+                difficulty: 100,
+                number: 0,  
+                timestamp: '--test-timestamp--',
+                nonce: 0,
+            }
+        })
+    })).toBe('028f5c28f5c28f60000000000000000000000000000000000000000000000000');
+});
+
 test(`Test Target Block Hash Calculation When Difficulty is MAX_HASH_VALUE`, () => {
     expect(Block.calculateBlockTargetHash({
         lastBlock: new Block({
