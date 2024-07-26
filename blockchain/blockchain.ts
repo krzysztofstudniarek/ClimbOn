e3c8b8fa4cb71a7afab460f560c0f673cb10bab9
Krzysztof Studniarek <krzysztof.studniarek@coinmetro.com>
2024-07-26 15:58:46 +0200

    feat: update addBlock

--- blockchain/blockchain.ts
+++ blockchain/blockchain.ts
@@ -7,7 +7,17 @@ export class Blockchain {
         this.chain = [Block.genesis()];
     }
 
-    addBlock({ block }: { block: Block }) {
-        this.chain.push(block);
+    addBlock({ block }: { block: Block }): Promise<void> {
+        return new Promise((resolve, reject) => {
+            Block.validateBlock({
+                lastBlock: this.chain[this.chain.length - 1],
+                block
+            }).then(() => {
+                this.chain.push(block);
+                resolve();
+            }).catch((error) => {
+                reject(error);
+            });
+        });
     }
 }
